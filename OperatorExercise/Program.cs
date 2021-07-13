f8a1df8a2a4eb9b34f81c51801d398cc49379b29
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-12 21:28:58 -0500

    Initial Commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            }
+        }
+    }
+}
